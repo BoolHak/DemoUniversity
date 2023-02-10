7eef7e4edc3d03e1460ffa5cb519aac61f026496
Godzilla <boolhak@gmail.com>
2023-02-10 20:02:42 +0100

    first commit

---
+++ DemoUniversity/Program.cs
@@ -0,0 +1,45 @@
+using DemoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<SchoolContext>();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+app.Run();
